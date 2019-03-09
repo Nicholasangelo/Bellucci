54fc82e39afdd659c7902ab8e72526cc34032bbd
Nicholas Angelo <40876154+Nicholasangelo@users.noreply.github.com>
2019-03-09 14:27:50 -0500

    angular route practice

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FirstCompComponent } from './components/homeComponent/first-comp.component';
+import { TopHeaderComponent } from './components/top-header/top-header.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+    { path: "home", component: TopHeaderComponent },
+  
+    {path: '',
+    redirectTo: "home",
+    pathMatch: "full"
+  
+  }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FirstCompComponent } from './components/first-comp/first-comp.component';
+import { FirstCompComponent } from './components/homeComponent/first-comp.component';
+import { TopHeaderComponent } from './components/top-header/top-header.component';
+
+
+const appRoutes: Routes = [
+  { path: "home", component: TopHeaderComponent },
+
+  {path: '',
+  redirectTo: "home",
+  pathMatch: "full"
+
+}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstCompComponent
+    FirstCompComponent,
+    TopHeaderComponent
   ],
   imports: [
     BrowserModule,
